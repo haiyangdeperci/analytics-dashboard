7a007b8906cbf32a8f8d93c48f682147a2a5cbda
haiyangdeperci <31899203+haiyangdeperci@users.noreply.github.com>
2021-01-14 17:41:56 +0100

    add time series support and periodicity

--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,9 @@ import pandas as pd
 GITHUB_API_REPO_URL = 'https://api.github.com/repos/activeloopai/Hub/'
 GITHUB_API_ORG_URL = 'https://api.github.com/orgs/activeloopai/'
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+__api_calls__ = 0
+START_DATETIME = pd.Timestamp(year=2019, month=8, day=1, tz='UTC')
+NOW = pd.Timestamp.now(tz='UTC')
 
 
 def getData(base, path, params=None):
@@ -88,6 +91,55 @@ def getIssues(state='all', labels=None):
     return bugs
 
 
+def overTime(data):
+    dr = pd.DataFrame({'key': -1, 'date': pd.date_range(
+        # workaround for the bug
+        # https://github.com/pandas-dev/pandas/issues/31930
+        START_DATETIME,
+        # ^^ data.created_at.min(),
+        NOW, freq='D'
+    )})
+    data['key'] = -1
+    data = data[['number', 'created_at', 'closed_at', 'key']]
+    merged = pd.merge(dr, data, on='key')
+    merged['count'] = np.where(
+        (merged.date > merged.created_at) & (
+            (merged.date < merged.closed_at) | pd.isnull(merged.closed_at)
+        ),
+        True, False
+    )
+
+    nOverTime = merged[merged['count']].groupby('date')['count'].count()
+    nOverTime = pd.concat(
+        [dr.set_index('date'), nOverTime], 1).drop('key', 1).fillna(0)
+    return nOverTime
+
+
+def total_hours(timedelta):
+    return round(timedelta.total_seconds() / 60 / 60, 2)
+
+
+def periodically(data, f='mean'):
+    # thresholds = [7, 30, 91]  # week, month, quarter
+    # periods = ['weekly', 'monthly', 'quarterly']
+    thresholds = {'weekly': 7, 'monthly': 30, 'quarterly': 91}
+    metrics = []
+    for name, t in thresholds.items():
+        current = NOW - pd.Timedelta(days=t), NOW
+        last = tuple(
+            dt - pd.Timedelta(days=t) for dt in current
+        )
+        current_metric = data[slice(*current)].agg(f).squeeze()
+        last_metric = data[slice(*last)].agg(f).squeeze()
+        diff_metric = (current_metric - last_metric) / last_metric
+        metrics.append({
+            'period': name,
+            f: current_metric,
+            'diff (%)': diff_metric * 100
+        })
+    return metrics
+
+
 open_bugs = getIssues('open', 'bug')
 st.write("Number of open bugs:", len(open_bugs))
 
@@ -98,3 +150,19 @@ st.write(
     f'{len(open_good_first_issues)} / {len(open_issues)} '
     f'({len(open_good_first_issues) / len(open_issues):.1%})',
 )
+
+
+closed_bugs = getIssues('closed', 'bug')
+closed_bugs[
+    'issue_resolution_time'
+] = closed_bugs['closed_at'] - closed_bugs['created_at']
+
+st.write(
+    'Average amount of time to close a bug ticket:',
+    closed_bugs.issue_resolution_time.mean().round('s')
+)
+
+
+closed_bugs['issue_resolution_time'] = closed_bugs[
+    'issue_resolution_time'
+].astype(np.int64)
