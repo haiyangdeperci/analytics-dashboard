5e6a8666dc73b259251d8db06fd53bad96102e47
haiyangdeperci <31899203+haiyangdeperci@users.noreply.github.com>
2021-01-14 15:09:23 +0100

    collect github issues

--- dashboard.py
+++ dashboard.py
@@ -28,3 +28,73 @@ def getData(base, path, params=None):
         if params['page'] > 20:
             raise Exception
     return results
+
+
+def parseNestedData(observation, key):
+    if isinstance(observation, list):
+        return [d[key] for d in observation]
+    if observation:
+        return observation[key]
+
+
+def isPullRequest(issue):
+    if issue:
+        return True
+    return False
+
+
+def getWhitelist():
+    members = getData(GITHUB_API_ORG_URL, 'members')
+    members = pd.DataFrame(members)
+    return members.login
+
+
+def getCommits():
+    commits = getData(GITHUB_API_REPO_URL, 'commits')
+    commits = pd.DataFrame(commits)
+    return commits
+
+
+def getIssues(state='all', labels=None):
+    params = {'state': state}
+    if labels:
+        params['labels'] = labels
+    bugs = getData(GITHUB_API_REPO_URL, 'issues', params)
+    bugs = pd.DataFrame(bugs)
+    # restrict columns to cols if they exist
+    cols = [
+        'id', 'node_id', 'number', 'title', 'user', 'state',
+        'locked', 'assignees', 'comments', 'created_at',
+        'updated_at', 'closed_at', 'author_association',
+        'pull_request'
+    ]
+    if not labels:
+        cols.append('labels')
+    intersected = bugs.columns.intersection(cols)
+    if 'pull_request' in intersected:
+        bugs['pull_request'] = pd.isnull(bugs['pull_request'])
+        bugs = bugs[bugs['pull_request']]
+    bugs = bugs[intersected]
+    bugs['author_id'] = bugs.user.apply(parseNestedData, args=('id',))
+    bugs['author_name'] = bugs.user.apply(parseNestedData, args=('login',))
+    bugs['assignee_ids'] = bugs.assignees.apply(parseNestedData, args=('id',))
+    bugs['assignee_names'] = bugs.assignees.apply(
+        parseNestedData, args=('login',)
+    )
+    bugs.drop(['user', 'assignees'], 1, inplace=True)
+    bugs[['created_at', 'updated_at', 'closed_at']] = bugs[[
+        'created_at', 'updated_at', 'closed_at'
+    ]].apply(pd.to_datetime, utc=True)
+    return bugs
+
+
+open_bugs = getIssues('open', 'bug')
+st.write("Number of open bugs:", len(open_bugs))
+
+open_issues = getIssues('open')
+open_good_first_issues = getIssues('open', 'good first issue')
+st.write(
+    "Number of good first issues / all issues:",
+    f'{len(open_good_first_issues)} / {len(open_issues)} '
+    f'({len(open_good_first_issues) / len(open_issues):.1%})',
+)
